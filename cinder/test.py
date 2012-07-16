0b3825a1032236300b41faad77f6ab54911ef9b9
Mark McLoughlin <markmc@redhat.com>
2012-07-16 21:10:17 +0100

    Use cfg's new behavior of reset() clearing overrides
    Cherry-picks b858108 from Nova.
    
    Change-Id: I5e14dea25afd5bff03a39671b2d3b85481977bec

--- cinder/test.py
+++ cinder/test.py
@@ -39,6 +39,7 @@ from cinder.openstack.common import cfg
 from cinder import utils
 from cinder import service
 from cinder import tests
+from cinder.tests import fake_flags
 
 
 test_opts = [
@@ -123,6 +124,9 @@ class TestCase(unittest.TestCase):
     def setUp(self):
         """Run before each test method to initialize test environment."""
         super(TestCase, self).setUp()
+
+        fake_flags.set_defaults(FLAGS)
+
         # NOTE(vish): We need a better method for creating fixtures for tests
         #             now that we have some required db setup for the system
         #             to work properly.
@@ -135,7 +139,6 @@ class TestCase(unittest.TestCase):
         self.stubs = stubout.StubOutForTesting()
         self.injected = []
         self._services = []
-        self._overridden_opts = []
 
     def tearDown(self):
         """Runs after each test method to tear down test environment."""
@@ -147,7 +150,7 @@ class TestCase(unittest.TestCase):
             super(TestCase, self).tearDown()
         finally:
             # Reset any overridden flags
-            self.reset_flags()
+            FLAGS.reset()
 
             # Stop any timers
             for x in self.injected:
@@ -173,17 +176,6 @@ class TestCase(unittest.TestCase):
         """Override flag variables for a test."""
         for k, v in kw.iteritems():
             FLAGS.set_override(k, v)
-            self._overridden_opts.append(k)
-
-    def reset_flags(self):
-        """Resets all flag variables for the test.
-
-        Runs after each test.
-
-        """
-        for k in self._overridden_opts:
-            FLAGS.set_override(k, None)
-        self._overridden_opts = []
 
     def start_service(self, name, host=None, **kwargs):
         host = host and host or uuid.uuid4().hex
